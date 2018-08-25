9f02892f1b211e716de6bab32f0bd2700aac209e
John Clark <john.clark@cantab.net>
2018-08-25 21:42:43 +0100

    Basic contact page to try and get something to work

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,92 +1,88 @@
-import React from 'react'
-import Layout from "../components/Layout"
-import { navigateTo } from 'gatsby'
-import { Container, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React from "react";
+import { navigateTo } from "gatsby-link";
 import Recaptcha from "react-google-recaptcha";
+import Layout from '../components/Layout';
 
 const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
+
 function encode(data) {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
-
 export default class Contact extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-    };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    handleRecaptcha = value => {
-        this.setState({ "g-recaptcha-response": value });
-    };
+  handleRecaptcha = value => {
+    this.setState({ "g-recaptcha-response": value });
+  };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        const form = e.target;
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({
-                "form-name": form.getAttribute("name"),
-                ...this.state
-            })
-        })
-            .then(() => navigateTo(form.getAttribute("action")))
-            .catch(error => alert(error));
-    };
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
 
-    render() {
-        return (
-            <Layout>
-                <Container>
-                    <h6 className='text-center'>
-                        Please use the form below to contact us and we will get back to you as soon as possible.
-                </h6>
-                    <br />
-                    <Form method="post"
-                        action="/thanks/"
-                        data-netlify="true"
-                        data-netlify-recaptcha="true"
-                        onSubmit={this.handleSubmit}>
-                        <FormGroup row>
-                            <Label for="name" sm={2}>Name</Label>
-                            <Col sm={10}>
-                                <Input type="text" name="name" id="name" />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Label for="email" sm={2}>Email</Label>
-                            <Col sm={10}>
-                                <Input type="email" name="email" id="email" />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Label for="message" sm={2}>Message</Label>
-                            <Col sm={10}>
-                                <Input type="textarea" name="message" id="message" />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row className='text-center'>
-                            <Recaptcha
-                                ref="recaptcha"
-                                sitekey={RECAPTCHA_KEY}
-                                onChange={this.handleRecaptcha}
-                            />
-                        </FormGroup>
-                        <FormGroup check row>
-                            <Col sm={{ size: 10, offset: 2 }}>
-                                <Button>Submit</Button>
-                            </Col>
-                        </FormGroup>
-                    </Form>
-                </Container>
-            </Layout>
-        )
-    }
+  render() {
+    return (
+      <Layout>
+        <h1>reCAPTCHA 2</h1>
+        <form
+          name="contact-recaptcha"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-recaptcha="true"
+          onSubmit={this.handleSubmit}
+        >
+          <noscript>
+            <p>This form won’t work with Javascript disabled</p>
+          </noscript>
+          <p>
+            <label>
+              Your name:<br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <Recaptcha
+            ref="recaptcha"
+            sitekey={RECAPTCHA_KEY}
+            onChange={this.handleRecaptcha}
+          />
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </Layout>
+    );
+  }
 }
