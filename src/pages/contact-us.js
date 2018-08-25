9ff7f4655604bc31fcfd4d2da41fd760815701a1
John Clark <john.clark@cantab.net>
2018-08-25 21:55:59 +0100

    Add some styling

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -2,6 +2,7 @@ import React from "react";
 import { navigateTo } from "gatsby-link";
 import Recaptcha from "react-google-recaptcha";
 import Layout from '../components/Layout';
+import {Container} from 'reactstrap';
 
 const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 
@@ -43,45 +44,44 @@ export default class Contact extends React.Component {
   render() {
     return (
       <Layout>
-        <h1>reCAPTCHA 2</h1>
-        <form
-          name="contact-recaptcha"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-recaptcha="true"
-          onSubmit={this.handleSubmit}
-        >
-          <noscript>
-            <p>This form won’t work with Javascript disabled</p>
-          </noscript>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <Recaptcha
-            ref="recaptcha"
-            sitekey={RECAPTCHA_KEY}
-            onChange={this.handleRecaptcha}
-          />
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        <Container>
+            <form
+            name="contact-recaptcha"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-recaptcha="true"
+            onSubmit={this.handleSubmit}
+            >
+            <noscript>
+                <p>This form won’t work with Javascript disabled</p>
+            </noscript>
+            <div className="form-group">
+                <label for="name">
+                Your name
+                </label>
+                <input type="text" name="name" className="form-control" onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+                <label for="email">
+                Your email
+                </label>
+                <input type="email" name="email" className="form-control" onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+                <label for="message">
+                Message
+                </label>
+                <textarea name="message" className="form-control" onChange={this.handleChange} />
+            </div>
+            <Recaptcha
+                ref="recaptcha"
+                sitekey={RECAPTCHA_KEY}
+                onChange={this.handleRecaptcha}
+            />
+            <button type="submit"  className="btn btn-primary">Send</button>
+            </form>
+        </Container>
       </Layout>
     );
   }
