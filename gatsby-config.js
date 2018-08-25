fdf069ba3a2beb02751b941fd635d42b8ea31e46
John Clark <john.clark@cantab.net>
2018-08-25 12:46:59 +0100

    Move back to an imprted CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,12 +33,6 @@ module.exports = {
         pathToConfigModule: `src/utils/typography.js`,
       },
     },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
