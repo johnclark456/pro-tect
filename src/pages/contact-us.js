2465b5de7f62f9bd727ffa4791c018ad9a3b481c
John Clark <john.clark@cantab.net>
2018-08-26 10:42:52 +0100

    Use netlify recaptcha

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -15,31 +15,31 @@ function encode(data) {
 export default class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "", email: "", message: "" };
+    // this.state = { name: "", email: "", message: "" };
   }
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  // handleChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
 
-  handleRecaptcha = value => {
-    this.setState({ "g-recaptcha-response": value });
-  };
+  // handleRecaptcha = value => {
+  //   this.setState({ "g-recaptcha-response": value });
+  // };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state
-      })
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch(error => alert(error));
-  };
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   const form = e.target;
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: encode({
+  //       "form-name": form.getAttribute("name"),
+  //       ...this.state
+  //     })
+  //   })
+  //     .then(() => navigate(form.getAttribute("action")))
+  //     .catch(error => alert(error));
+  // };
 
   render() {
     const { name, email, message } = this.state;
@@ -52,7 +52,7 @@ export default class Contact extends React.Component {
             action="/thanks/"
             data-netlify="true"
             netlify-honeypot="bot-field"
-            onSubmit={this.handleSubmit}
+            // onSubmit={this.handleSubmit}
             >
             <noscript>
                 <p>This form won’t work with Javascript disabled</p>
@@ -62,19 +62,19 @@ export default class Contact extends React.Component {
                 <label htmlFor="name">
                 Your name
                 </label>
-                <input type="text" name="name" className="form-control"  value={name} onChange={this.handleChange} />
+                <input type="text" name="name" className="form-control" />
             </div>
             <div className="form-group">
                 <label htmlFor="email">
                 Your email
                 </label>
-                <input type="email" name="email" className="form-control"  value={email} onChange={this.handleChange} />
+                <input type="email" name="email" className="form-control" />
             </div>
             <div className="form-group">
                 <label htmlFor="message">
                 Message
                 </label>
-                <textarea name="message" className="form-control"  value={message} onChange={this.handleChange} />
+                <textarea name="message" className="form-control" />
             </div>
             <div data-netlify-recaptcha />
             {/* <Recaptcha
