078800afe52abbecf8488f6dadea0e4f606d899f
John Clark <john.clark@cantab.net>
2018-09-02 14:24:57 +0100

    Add templates to cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,15 @@
 import CMS from "netlify-cms";
+import AboutPagePreview from './preview-templates/AboutPagePreview'
+import BlogPostPreview from './preview-templates/BlogPostPreview'
 
 // import my homemade widget
 import { CustomPathImageControl, CustomPathImagePreview } from "./customPathImage.js";
+
+CMS.registerPreviewTemplate('about', AboutPagePreview)
+
+CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+
 
 // register it to be able tu use it in NetlifyCMS
 CMS.registerWidget("custompathimage", CustomPathImageControl, CustomPathImagePreview);
