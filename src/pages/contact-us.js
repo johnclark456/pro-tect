b4f454881da558e23d47c09fe26466fa01e9abea
Johnathon Clark <john.clark@cantab.net>
2022-05-05 17:27:23 +0100

    Reformat contact us page

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { navigate } from "gatsby-link";
 import Recaptcha from "react-google-recaptcha";
-import Layout from '../components/Layout';
-import {Container} from 'reactstrap';
+import Layout from "../components/Layout";
+import { Container } from "reactstrap";
 
 const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 
 function encode(data) {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
     .join("&");
 }
 
@@ -18,15 +18,15 @@ export default class Contact extends React.Component {
     this.state = {};
   }
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  handleRecaptcha = value => {
+  handleRecaptcha = (value) => {
     this.setState({ "g-recaptcha-response": value });
   };
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
     fetch("/", {
@@ -34,18 +34,18 @@ export default class Contact extends React.Component {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
         "form-name": form.getAttribute("name"),
-        ...this.state
-      })
+        ...this.state,
+      }),
     })
       .then(() => navigate(form.getAttribute("action")))
-      .catch(error => alert(error));
+      .catch((error) => alert(error));
   };
 
   render() {
     return (
       <Layout>
         <Container>
-            <form
+          <form
             name="contact-us"
             method="post"
             action="/thanks/"
@@ -53,42 +53,52 @@ export default class Contact extends React.Component {
             data-netlify-recaptcha="true"
             data-netlify-honeypot="bot-field"
             onSubmit={this.handleSubmit}
-            >
+          >
             <noscript>
-                <p>This form won’t work with Javascript disabled</p>
+              <p>This form won’t work with Javascript disabled</p>
             </noscript>
-          <input type="hidden" name="form-name" value="contact-us" />
-          <p hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </p>
+            <input type="hidden" name="form-name" value="contact-us" />
+            <p hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={this.handleChange} />
+              </label>
+            </p>
             <div className="form-group">
-                <label htmlFor="name">
-                Your name
-                </label>
-                <input type="text" name="name" className="form-control" onChange={this.handleChange} />
+              <label htmlFor="name">Your name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                onChange={this.handleChange}
+              />
             </div>
             <div className="form-group">
-                <label htmlFor="email">
-                Your email
-                </label>
-                <input type="email" name="email" className="form-control" onChange={this.handleChange} />
+              <label htmlFor="email">Your email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                onChange={this.handleChange}
+              />
             </div>
             <div className="form-group">
-                <label htmlFor="message">
-                Message
-                </label>
-                <textarea name="message" className="form-control" onChange={this.handleChange} />
+              <label htmlFor="message">Message</label>
+              <textarea
+                name="message"
+                className="form-control"
+                onChange={this.handleChange}
+              />
             </div>
             <Recaptcha
-                ref="recaptcha"
-                sitekey={RECAPTCHA_KEY}
-                onChange={this.handleRecaptcha}
+              ref="recaptcha"
+              sitekey={RECAPTCHA_KEY}
+              onChange={this.handleRecaptcha}
             />
-            <button type="submit"  className="btn btn-primary">Send</button>
-            </form>
+            <button type="submit" className="btn btn-primary">
+              Send
+            </button>
+          </form>
         </Container>
       </Layout>
     );
